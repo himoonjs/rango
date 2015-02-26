6b41fb305b1ab4282ebc911d71a8c26bebe89ba5
hattuni <himoonjs@gmail.com>
2015-02-26 21:44:32 +0200

    7. Models, Templates and Views

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -13,21 +13,25 @@ def populate():
 
 	add_page(cat=python_cat,
 		title="Official Python Tutorial",
-		url="http://docs.python.org/2/tutorial/")
+		url="http://docs.python.org/2/tutorial/",
+		views=84)
 
 	add_page(cat=python_cat,
 		title="How to Think like a Computer Scientiest",
-		url="http://www.greenteapress.com/thinkpython/")
+		url="http://www.greenteapress.com/thinkpython/",
+		views=6)
 
 	django_cat = add_cat('Django', views=64, likes=32)
 
 	add_page(cat=django_cat, 
 		title="Official Django Tutorial", 
-		url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
+		url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/",
+		views=21)
 
 	add_page(cat=django_cat,
 		title="Django Rocks",
-		url="http://www.djangorocks.com/")
+		url="http://www.djangorocks.com/",
+		views=56)
 
 	add_page(cat=django_cat,
 		title="How to Tango with Django",
@@ -37,11 +41,13 @@ def populate():
 
 	add_page(cat=frame_cat,
 		title="Bottle",
-		url="http://bottlepy.org/docs/dev/")
+		url="http://bottlepy.org/docs/dev/",
+		views=27)
 
 	add_page(cat=frame_cat,
 		title="Flask",
-		url="http://flask.pocoo.org")
+		url="http://flask.pocoo.org",
+		views=34)
 
 	# Print out what we have added to the user.
 	for c in Category.objects.all():
@@ -49,15 +55,15 @@ def populate():
 			print "- {0} - {1}".format(str(c), str(p))
 
 def add_page(cat, title, url, views=0):
-	p = Page.objects.get_or_create(category=cat, title=title)[0]
-	p.url=url
-	p.views=views
-	p.save()
-	return p
+    p = Page.objects.get_or_create(category=cat, title=title, url=url, views=views)[0]
+    return p
 
 def add_cat(name, views, likes):
-	c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
-	return c
+    c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
+    c.save()
+    return c
 
 # Start execution here!
 if __name__== '__main__':
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rango.models import Category
+from rango.models import Page
+
 def index(request):
+	# Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary which will be passed to the template engine.
 
-	# Construct a dictionary to pass to the template engine as its context.
-	# Note the key boldmessage is the same as {{ boldmessage }} in the template!
-	context_dict = {'boldmessage': "I am bold font from the context"}
+    context_dict = {}
 
-	# Return a rendered response to send to the client.
-	# We make use of the shortcut fuction to make our lives easier.
-	# Note that the first parameter is the template we wish to use.
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict['categories'] = category_list
 
-	return render(request, 'rango/index.html', context_dict)
+    most_viewd_page_list = Page.objects.order_by('-views')[:5]
+    context_dict['most_viewd_pages'] = most_viewd_page_list
+
+    # Render the response and send it back!
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
 
@@ -19,3 +27,18 @@ def about(request):
 
 	return render(request, 'rango/about.html', context_dict)
 	#return HttpResponse("Rango says here is the about page.<br/><a href='/rango/'>Index</a>")
+
+def category(request, category_name_slug):
+
+	context_dict = {}
+
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		context_dict['category_name'] = category.name
+
+		pages = Page.objects.filter(category=category)
+		context_dict['pages'] = pages
+		context_dict['category'] = category
+	except Category.DoesNotExist:
+		pass
+	return render(request, 'rango/category.html', context_dict)
